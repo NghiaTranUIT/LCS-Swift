a123201acf8af5407ce264d89fe9db8aa9023b7f
Nghia Tran <vinhnghiatran@gmail.com>
2017-02-18 15:27:44 +0700

    Finished LCS with DP approach. Support any kind of array

--- LCS-Dynamic-Programming-Part-1.playground/Contents.swift
+++ LCS-Dynamic-Programming-Part-1.playground/Contents.swift
@@ -24,30 +24,46 @@ extension Array where Element: Equatable {
         let table = MemorizationTable.buildTable(x: self, y: other)
         
         // Print LCS
-        return self.lcsFromMemorizationTable(table)
+        return self.lcsFromMemorizationTable(table, self, other, self.count, other.count)
     }
     
-    fileprivate func lcsFromMemorizationTable(_ table: [[Int]]) -> [Element] {
+    fileprivate func lcsFromMemorizationTable(_ table: [[Int]], _ x: [Element], _ y: [Element], _ i: Int, _ j: Int) -> [Element] {
         
+        // Exit
+        if i == 0 || j == 0 {
+            return []
+        }
+        
+        // Recurvise 
+        else if x[i-1] == y[j-1] { // MATCH -> Get element
+            return lcsFromMemorizationTable(table, x, y, i - 1, j - 1) + [x[i-1]]
+        }
+        else if table[i-1][j] > table[i][j-1] { // Top
+            return lcsFromMemorizationTable(table, x, y, i - 1, j)
+        }
+        else { // Left
+            return lcsFromMemorizationTable(table, x, y, i, j - 1)
+        }
     }
 }
 
+// Memorization table
 struct MemorizationTable<T: Equatable> {
     
     static func buildTable(x: [T], y: [T]) -> [[Int]] {
         
         // Create 2-dimension table, and filled with zero
-        let m = x.count
-        let n = y.count
+        let n = x.count
+        let m = y.count
         var table = Array(repeating: Array(repeating: 0, count: m + 1), count: n + 1)
-        
+    
         // Iterate from top-left corner -> bottom-right corner
         for i in 0...n {
             for j in 0...m {
                 if i == 0 || j == 0 {
                     table[i][j] = 0
                 }
-                else if x[i - 1] == y[j-1] { // MATCH
+                else if x[i-1] == y[j-1] { // MATCH
                     table[i][j] = table[i-1][j-1] + 1
                 }
                 else { // NOT MATCH
@@ -59,3 +75,40 @@ struct MemorizationTable<T: Equatable> {
         return table
     }
 }
+
+
+////////////////////////
+////// TEST with string
+let a = "acbaed".characters.map { char -> String in
+    return String(char)
+}
+let b = "abcadfa".characters.map { char -> String in
+    return String(char)
+}
+let lcs = a.LCS(b) // ["a", "b", "a", "d"]
+
+
+
+//////////////////////////
+// Test with custom model
+struct UserObj: CustomStringConvertible {
+    let name: String!
+
+    var description: String {
+        return "{\(self.name!)}"
+    }
+}
+
+// Adopt Equatable
+extension UserObj: Equatable {
+    public static func ==(lhs: UserObj, rhs: UserObj) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
+
+let localUsers = [UserObj(name: "Nghia Tran"), UserObj(name: "nghiatran.me"), UserObj(name: "SaiGon"), UserObj(name: "Algorithm")]
+let remoteUsers = [UserObj(name: "Kamakura"), UserObj(name: "Nghia Tran"), UserObj(name: "Algorithm"), UserObj(name: "SaiGon"), UserObj(name: "Somewhere")]
+
+// LCS
+let lcsUser = localUsers.LCS(remoteUsers)
+print(lcsUser) // "[{Nghia Tran}, {Algorithm}]"
